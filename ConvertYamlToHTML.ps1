b3d9d12fac4b4a9f7271583f4e0ab986f91373ee
Jared Atkinson <jaredcatkinson@users.noreply.github.com>
2020-05-13 05:55:56 -0700

    Update ConvertYamlToHTML.ps1

--- ConvertYamlToHTML.ps1
+++ ConvertYamlToHTML.ps1
@@ -1,68 +1,64 @@
 foreach ($yamlfile in (Get-ChildItem *.yaml))
 {
+    $yaml = ConvertFrom-Yaml -Path $yamlfile.FullName
 
-ConvertFrom-Yaml -Path $yamlfile.FullName
+    $sb = [System.Text.StringBuilder]::new()
 
-@"
-<html>
-    <head>
-        <style>
-            table {
-                width: 60%;
-                margin: 0 auto;
-                background-color: #000000;
-            }
+    [void]$sb.AppendLine('<html>')
+    [void]$sb.AppendLine('    <head>')
+    [void]$sb.AppendLine('        <style>')
+    [void]$sb.AppendLine('            table {')
+    [void]$sb.AppendLine('                width: 60%;')
+    [void]$sb.AppendLine('                margin: 0 auto;')
+    [void]$sb.AppendLine('                background-color: #000000;')
+    [void]$sb.AppendLine('            }')
+    [void]$sb.AppendLine('')
+    [void]$sb.AppendLine('            th {')
+    [void]$sb.AppendLine('                background-color: #DCDCDC;')
+    [void]$sb.AppendLine('            }')
+    [void]$sb.AppendLine('')
+    [void]$sb.AppendLine('            table,')
+    [void]$sb.AppendLine('            th,')
+    [void]$sb.AppendLine('            td {')
+    [void]$sb.AppendLine('                border: 3px solid black;')
+    [void]$sb.AppendLine('                border-collapse: collapse;')
+    [void]$sb.AppendLine('                font-weight: bold;')
+    [void]$sb.AppendLine('            }')
+    [void]$sb.AppendLine('')
+    [void]$sb.AppendLine('            th,')
+    [void]$sb.AppendLine('            td {')
+    [void]$sb.AppendLine('                padding: 15px;')
+    [void]$sb.AppendLine('                text-align: center;')
+    [void]$sb.AppendLine('            }')
+    [void]$sb.AppendLine('        </style>')
+    [void]$sb.AppendLine('     </head>')
+    [void]$sb.AppendLine('     <body>')
+    [void]$sb.AppendLine('         <table>')
+    [void]$sb.AppendLine('             <tr>')
+    [void]$sb.AppendLine("                 <th colspan=`"$($yaml.header.colspan)`" id=`"th01`">$($yaml.header.name)</th>")
+    [void]$sb.AppendLine('             </tr>')
 
-            th {
-                background-color: #DCDCDC
-            }
-
-            table,
-            th,
-            td {
-                border: 3px solid black;
-                border-collapse: collapse;
-                font-weight: bold;
-            }
-
-            th,
-            td {
-                padding: 15px;
-                text-align: center;
-            }
-        </style>
-    </head>
-"@ | Out-File -FilePath "$($yamlfile.Name.Split('.')[0]).html" 
-
-@"
-    <body>
-        <table>
-            <tr>
-                <th colspan="$($yaml.header.colspan)" id="th01">$($yaml.header.name)</th>
-            </tr>
-"@ | Out-File -FilePath "$($yamlfile.Name.Split('.')[0]).html" -Append
-
-foreach($row in $yaml.rows)
-{
-    "            <tr>" | Out-File -FilePath "$($yamlfile.Name.Split('.')[0]).html" -Append
-    "                <td style=`"background-color:`#FFFFFF`">$($row.name)</td>" | Out-File -FilePath "$($yamlfile.Name.Split('.')[0]).html" -Append
-
-    $red = [string]::Format("{0:X2}", $row.style.red)
-    $green = [string]::Format("{0:X2}", $row.style.green)
-    $blue = [string]::Format("{0:X2}", $row.style.blue)
-    $backgroundcolor = "$($red)$($green)$($blue)"
-
-    foreach($entry in $row.entries)
+    foreach($row in $yaml.rows)
     {
-        "                <td colspan=$($entry.attributes.colspan) style=`"background-color:`#$($backgroundcolor)`">$($entry.name)</td>" | Out-File -FilePath "$($yamlfile.Name.Split('.')[0]).html" -Append
+        [void]$sb.AppendLine('            <tr>')
+        [void]$sb.AppendLine("                <td style=`"background-color: #FFFFFF`">$($row.name)</td>")
+
+        $red = [string]::Format("{0:X2}", $row.style.red)
+        $green = [string]::Format("{0:X2}", $row.style.green)
+        $blue = [string]::Format("{0:X2}", $row.style.blue)
+        $backgroundcolor = "$($red)$($green)$($blue)"
+
+        foreach($entry in $row.entries)
+        {
+            [void]$sb.AppendLine("                <td colspan=$($entry.attributes.colspan) style=`"background-color: #$($backgroundcolor)`">$($entry.name)</td>")
+        }
+
+        [void]$sb.AppendLine('            </tr>')
     }
 
-    "            </tr>" | Out-File -FilePath "$($yamlfile.Name.Split('.')[0]).html" -Append
-}
+    [void]$sb.AppendLine('        </table>')
+    [void]$sb.AppendLine('    </body>')
+    [void]$sb.AppendLine('</html>')
 
-@"
-        </table>
-    </body>
-</html>
-"@ | Out-File -FilePath "$($yamlfile.Name.Split('.')[0]).html" -Append
+    $sb.ToString() | Out-File -FilePath "$($yamlfile.Name.Split('.')[0]).html"
 }
